a4b052549ef54cb06f1bd76ed1e494137de3791d
Godwin Burkhardt <godwin.burkhardt@isc-konstanz.de>
2018-05-11 12:16:43 +0200

    Command to switch on/off changed

---
+++ projects/homematic/src/main/java/org/ogema/driver/homematic/manager/asksin/devices/SwitchPlug.java
@@ -0,0 +1,23 @@
+package org.ogema.driver.homematic.manager.asksin.devices;
+
+import org.ogema.core.channelmanager.measurements.BooleanValue;
+import org.ogema.core.channelmanager.measurements.Value;
+import org.ogema.driver.homematic.manager.RemoteDevice;
+
+public class SwitchPlug extends org.ogema.driver.homematic.manager.devices.SwitchPlug {
+
+	public SwitchPlug(RemoteDevice rd) {
+		super(rd);
+	}
+
+	@Override
+	public void channelChanged(byte identifier, Value value) {
+		if (identifier == 0x01) {
+			BooleanValue v = (BooleanValue)value;
+			this.remoteDevice.pushCommand((byte) 0xA0, (byte) 0x11, 
+					"0201" + ((v.getBooleanValue()) ? "C8" : "00") + "0000");
+			// Get state
+			this.remoteDevice.pushCommand((byte) 0xA0, (byte) 0x01, "010E");
+		}
+	}
+}
